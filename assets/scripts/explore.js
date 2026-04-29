a93fcc9209388a2322034127d7a34845d0e1f158
Andre Lew <118043566+aelew@users.noreply.github.com>
2026-04-28 18:05:18 -0700

    explore: update speech synthesis js

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,5 +3,44 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  if (typeof speechSynthesis === 'undefined') {
+    alert('Speech synthesis is not supported in this browser!');
+    return;
+  }
+
+  const voiceSelectEl = document.getElementById('voice-select');
+  const textToSpeakEl = document.getElementById('text-to-speak');
+
+  const faceImageEl = document.querySelector('#explore > img');
+  const talkButtonEl = document.querySelector('#explore > button');
+
+  const voices = new Map();
+
+  speechSynthesis.addEventListener('voiceschanged', (e) => {
+    e.target.getVoices().forEach((voice) => {
+      const option = document.createElement('option');
+      option.textContent = voice.name;
+      option.value = voice.name;
+
+      voiceSelectEl.appendChild(option);
+      voices.set(voice.name, voice);
+    });
+  });
+
+  talkButtonEl.addEventListener('click', (e) => {
+    const utterance = new SpeechSynthesisUtterance(textToSpeakEl.value);
+    utterance.voice = voices.get(voiceSelectEl.value);
+
+    utterance.addEventListener('start', () => {
+      faceImageEl.src = 'assets/images/smiling-open.png';
+      faceImageEl.alt = 'Smiling face with open mouth';
+    });
+
+    utterance.addEventListener('end', () => {
+      faceImageEl.src = 'assets/images/smiling.png';
+      faceImageEl.alt = 'Smiling face';
+    });
+
+    speechSynthesis.speak(utterance);
+  });
 }
